b923e2b49dc9915f0012b71e2291edd7f8d6b316
Max Hsu <maxhsu1@gmail.com>
2019-05-15 18:33:13 -0700

    modified output file path

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
   entry:__dirname + '/client/src/index.jsx',
   output:{
     filename: 'bundle.js',
-    path:__dirname + 'client/dist'
+    path:__dirname + '/client/dist'
   },
   module:{
     rules:[{
