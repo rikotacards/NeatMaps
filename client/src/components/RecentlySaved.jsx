525ef8d33901936549f2ff1731b589be43b24e92
Max Hsu <maxhsu1@gmail.com>
2019-05-15 23:46:06 -0700

    added button for recently saved data

---
+++ client/src/components/RecentlySaved.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RecentlySaved extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <button>data set 1</button>
+      </div>
+    )
+  }
+}
+
+export default RecentlySaved
