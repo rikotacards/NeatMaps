1f14b3bba731c73b8f704bdd4cbefc4aa39fac53
Max Hsu <maxhsu1@gmail.com>
2019-05-15 18:26:32 -0700

    completed webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry:__dirname + '/client/src/index.jsx',
+  output:{
+    filename: 'bundle.js',
+    path:__dirname + 'client/dist'
+  },
+  module:{
+    rules:[{
+      test:[/\.jsx$/],
+      exclude:/node_modules/,
+      use:{
+        loader:'babel-loader',
+        options:{
+          presets:['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }
+
+    ]
+  }
+}
