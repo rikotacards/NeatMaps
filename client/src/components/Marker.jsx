ffe15d5713c2fa68311c86c61b1432346ec935a7
Max Hsu <maxhsu1@gmail.com>
2019-05-16 17:25:10 -0700

    added dynamic background color based on category

--- client/src/components/Marker.jsx
+++ client/src/components/Marker.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+//import Marker from 'google-map-react';
 
 var Marker = (props) => {
+  var divStyle = {
+    color: 'white',
+    background: `#${props.colorCode}`,
+    padding: '10px 5px',
+    display: 'inline-flex',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize:'7px',
+
+
+  }
   return(
-    <div>
-      {this.props.text}
+    <div style={divStyle}>
+      {props.text}
     </div>
   )
 }
