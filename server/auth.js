afc049ba9bd1ccfcabe7e3f75ee858a702527965
Max Hsu <maxhsu1@gmail.com>
2019-05-18 16:47:41 -0700

    added auth function to check user against neat api

--- server/auth.js
+++ server/auth.js
@@ -2,26 +2,24 @@ var axios = require('axios')
 
 
 
-var auth = () => {
+var auth = async(email, password) => {
+  try{
 
-  axios({
-    url:"http://neat-mvp-api.herokuapp.com/v1/auth",
-    method:'POST',
-    headers:{
-      'email':'neatmaps@gmail.com',
-      'password':12345
-    }
-  })
-  .then((res)=> {
-    console.log(res.data)
-  })
-  .catch((error)=>{
-    console.log('error', error.config)
-  })
+    var path = "http://neat-mvp-api.herokuapp.com/v1/auth"
+
+    var result = await axios({
+      url: path,
+      method:'POST',
+      data:`email=${email}&password=${password}`})
+      console.log(result)
+      return result
+
+  } catch(error){
+      console.log('unsucesful')
+  }
 }
 
-auth()
 
-// '{"id":"user_5YH84sGPej4n43o","created_at":1558122533,"email":"neatmaps@gmail.com","first_name":null,"last_name":null,"admin":true,"account":{"id":153,"uuid":"acc_RFbnbtDXDhg9hoYf","created_at":"2019-05-17 19:48:53 +0000","balance":"0.3E4","name":"1","status":"active","livemode":true,"user_id":155,"card_number":"1111222233334444"}}'
-
-// `{"id":"user_5YH84sGPej4n43o","created_at":1558122533,"email":"neatmaps@gmail.com","first_name":null,"last_name":null,"admin":true,"account":{"id":153,"uuid":"acc_RFbnbtDXDhg9hoYf","created_at":"2019-05-17 19:48:53 +0000","balance":"0.3E4","name":"1","status":"active","livemode":true,"user_id":155,"card_number":"1111222233334444"}}`
+module.exports = {
+  auth:auth
+}
