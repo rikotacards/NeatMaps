59d4c57989d9f9933c2c75613d424019471997a5
Max Hsu <maxhsu1@gmail.com>
2019-05-17 22:31:58 -0700

    added router wrappers

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.jsx'
+import App from './components/App.jsx'
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+
+<App/>, document.getElementById('app'));
