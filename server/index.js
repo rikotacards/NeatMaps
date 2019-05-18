645ac4604f8f701b86097937f6e03b0e5eca6a2c
Max Hsu <maxhsu1@gmail.com>
2019-05-18 16:48:12 -0700

    added authentication endpoint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser')
@@ -8,8 +9,10 @@ var upload = multer({dest:'temp/csv/'})
 var process = require('./ProcessData.js')
 var Geocoding = require('./Geocoding.js')
 
-app.use(express.static(path.join(__dirname, './../client/dist')))
+var auth = require('./auth.js')
 
+app.use(express.static(path.join(__dirname, './../client/dist')))
+app.use(cors())
 app.use(bodyParser.json());
 
 
@@ -31,22 +34,22 @@ app.post('/uploadcsv',upload.single('file'), (req,res) => {
 app.post('/mapdata', upload.none(), async(req,res) => {
   try{
     var headerMapping = req.body
-    console.log(headerMapping)
-    console.log('server filepath', filePath)
+
     var data = await process.mapHeader(headerMapping, filePath)
     var dataWithCoordinates = await Geocoding.convertToGeocode(data)
 
-
-    console.log(dataWithCoordinates)
     res.status(200).json(dataWithCoordinates)
-
-
 
   } catch(err){
     console.log(err)
   }
 })
 
+app.post('/authenticate',async(req,res) =>{
+  var email = req.body.email
+  var password = req.body.password
+  console.log('here', await auth.auth(email,password))
+})
 
 app.listen(port,(err,result) => {
   if(err){
