04555bbea69f78841b3ab08886bc5acd9ee480cc
Max Hsu <maxhsu1@gmail.com>
2019-05-25 08:22:23 -0700

    removed extra file, modified styling of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
-module.exports = {
-  mode:'development',
-  entry:__dirname + '/client/src/index.jsx',
-  output:{
-    filename: 'bundle.js',
-    path:__dirname + '/client/dist',
-  },
-  module:{
-    rules:[{
-      test:[/\.jsx$/],
-      exclude:/node_modules/,
-      use:{
-        loader:'babel-loader',
-      // options moved to .babelrc file in root
-      }
-    }
+/* eslint-disable prefer-template */
+/* eslint-disable no-path-concat */
 
-    ]
-  }
-}
+module.exports = {
+  mode: 'development',
+  // eslint-disable-next-line no-path-concat
+  entry: __dirname + '/client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist',
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx$/],
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      // options moved to .babelrc file in root
+      },
+    },
+    ],
+  },
+};
